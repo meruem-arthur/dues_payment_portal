571b5e1acbe01936e266fddde139ccdb4b9e6895
Meruem <meruemarthur@gmail.com>
2026-08-19 23:38:16 +0000

    student page css fix

--- src/app/d/[departmentSlug]/page.tsx
+++ src/app/d/[departmentSlug]/page.tsx
@@ -56,7 +56,7 @@ export default async function PublicDepartmentPage({ params }: { params: { depar
           )}
         </div>
 
-        <div className="grid grid-cols-2 gap-10 sm:gap-48 mt-4 sm:mt-6">
+        <div className="grid grid-cols-2 items-start gap-10 sm:gap-48 mt-4 sm:mt-6">
           <DuesCard
             title="First Year Students"
             amount={Number(department.fresherAmount)}
